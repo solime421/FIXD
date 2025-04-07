493786fdb1d1b2166f3d0c1671345e31b9f743ab
solime421 <tamara.solime1@gmail.com>
2025-04-07 19:42:43 +0300

    testing git

--- src/server.js
+++ src/server.js
@@ -61,5 +61,6 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
+// port specified in .env
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
